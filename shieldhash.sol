8ea745ade7f621bc87c146381a363a86849708c5
Rahul Sharma <66682591+rahulsharma0856@users.noreply.github.com>
2021-04-25 21:57:29 +0530

    Create shieldhash.sol

---
+++ shieldhash.sol
@@ -0,0 +1,38 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+contract ShieldHash {
+    struct data {
+        string hashinfo;
+        address user;
+        uint timestamp;
+    }
+
+    mapping(address => data[]) sOwner;
+
+    // Send a hashinfo ( Four-Tier Encrypted Data ) to a owner box 
+    // _sOwner :: sender address, _hashinfo :: Encrypted Data 
+    function setHashinfo(address _sOwner, string memory _hashinfo) external {
+        
+        require(bytes(_hashinfo).length >= 10, "data length is too small.");
+       
+        data memory message = data(_hashinfo, msg.sender, block.timestamp);
+        sOwner[_sOwner].push(message);
+    }
+
+    // Currently, there is no support for returning nested lists, so the length
+    // of hashinfo needs to be fetched and then retrieved by index. This is not
+    // fast but it is the most gas efficient method for storing and
+    // fetching data. Ideally this only needs to be done once per owner box load
+    function getHashinfoCountForOwner(address _sOwner) external view returns (uint) {
+        return sOwner[_sOwner].length;
+    }
+
+    // There is no support for returning a struct to web3, so this needs to be
+    // returned as multiple items. This will throw an error if the index from 
+    // owner box is invalid
+    function getHashinfoByIndexForOwner(address _sOwner, uint _index) external view returns (string memory, address, uint) {
+        data memory message = sOwner[_sOwner][_index];
+        return (message.hashinfo, message.user, message.timestamp);
+    }
+
+}
